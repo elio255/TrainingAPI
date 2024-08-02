7b1bdae56c9a2ce8f32ea0a7d6034e17307a8f4c
elio255 <eliomattar00@gmail.com>
2024-08-02 02:50:05 -0900

    adding new endpoint

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ console.log(process.env.ALI)
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
+  app.get('/students', function (req, res) {
+   var data = [
+    {id:1, name:'Mhmd Mhmd'},
+    {id:2, name:'dania'},
+    {id:3, name:'Ali'},
+    {id:4, name:'Rita'}
+]
+    res.send(data)
+  })
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
