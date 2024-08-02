ec2f2e37c97712638cc6f01632a9436eedb2d92f
elio255 <eliomattar00@gmail.com>
2024-08-02 01:56:12 -0900

    C1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT || 3030;
+console.log(process.env.ALI)
+// your code
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
